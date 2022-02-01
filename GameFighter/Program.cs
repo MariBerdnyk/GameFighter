390fdd0fab7bcb3431321e5986a22604ea914ed6
BadWolf03 <tspam765@gmail.com>
2022-02-01 14:01:50 +0200

    Добавьте файлы проекта.

---
+++ GameFighter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameFighter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
