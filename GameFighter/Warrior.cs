276b9a80c644d090019d17d6a9926112856d0e2e
BadWolf03 <tspam765@gmail.com>
2022-02-01 14:18:02 +0200

    make warrior

--- GameFighter/Warrior.cs
+++ GameFighter/Warrior.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace GameFighter
 {
-    class Warrior
+    public class Warrior
     {
+        public int health = default;
+        private int attack = default;
+        public bool Alive { get; set; }
+        public Warrior()
+        {
+            health = 50;
+            attack = 5;
+            Alive = true;
+        }
+        public bool Fight(Warrior warrior)
+        {
+            while(warrior.health > 0 && health > 0)
+            {
+                warrior.health -= attack;
+                health -= warrior.attack;
+                Alive = health > 0;
+            }
+            return Alive;
+        }
     }
 }
