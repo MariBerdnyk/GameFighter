32b532036a18094a9dca4ef4da6f3e62696a81f6
BadWolf03 <tspam765@gmail.com>
2022-02-02 21:09:16 +0200

    first test

--- GameFighterXUnitTests/GameFighterTests.cs
+++ GameFighterXUnitTests/GameFighterTests.cs
@@ -1,3 +1,4 @@
+using GameFighter;
 using System;
 using Xunit;
 
@@ -6,9 +7,14 @@ namespace GameFighterXUnitTests
     public class GameFighterTests
     {
         [Fact]
-        public void Test1()
+        public void Fight_Two_Warriors()
         {
+            var warrior_1 = new Warrior();
+            var warrior_2 = new Warrior();
 
+            Battle.Fight(warrior_1, warrior_2);
+
+            Assert.True(Battle.Fight(warrior_1, warrior_2));
         }
     }
 }
