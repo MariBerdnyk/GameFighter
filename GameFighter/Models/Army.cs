b5d87458fffd600619371d89e97ba0e3b664de63
BadWolf03 <tspam765@gmail.com>
2022-02-05 11:45:27 +0200

    Properties

--- GameFighter/Models/Army.cs
+++ GameFighter/Models/Army.cs
@@ -8,5 +8,30 @@ namespace GameFighter.Models
 {
     public class Army
     {
+        public List<Warrior> ArmyMembers { get; protected set; }
+
+        public int CountUnits => ArmyMembers.Count;
+
+        public int CountAliveUnits
+        {
+            get
+            {
+                int alive = 0;
+                foreach (var item in ArmyMembers)
+                {
+                    if (item.IsAlive)
+                    {
+                        alive++;
+                    }
+                }
+                return alive;
+            }
+        }
+
+        public Army()
+        {
+            ArmyMembers = new List<Warrior>();
+        }
+        
     }
 }
