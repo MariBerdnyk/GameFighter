53d66acf97b552e5f1da0aa8dacbb880ca36573a
BadWolf03 <tspam765@gmail.com>
2022-02-07 21:46:57 +0200

    change conflict

--- GameFighter/Program.cs
+++ GameFighter/Program.cs
@@ -7,28 +7,26 @@ namespace GameFighter
     {
         static void Main(string[] args)
         {
-            //var chuck = new Warrior();
-            //var bruce = new Warrior();
-            //var carl = new Knight();
-            //var dave = new Warrior();
+            var chuck = new Warrior();
+            var bruce = new Warrior();
+            var carl = new Knight();
+            var dave = new Warrior();
 
 
-            //Console.WriteLine(Battle.Fight(chuck, bruce));
-            //Console.WriteLine(Battle.Fight(dave, carl));
+            Console.WriteLine(Battle.Fight(chuck, bruce));
+            Console.WriteLine(Battle.Fight(dave, carl));
 
-            //Console.WriteLine("Are alive:");
-            //Console.WriteLine("\t" + chuck.IsAlive);
-            //Console.WriteLine("\t" + bruce.IsAlive);
-            //Console.WriteLine("\t" + carl.IsAlive);
-            //Console.WriteLine("\t" + dave.IsAlive);
+            Console.WriteLine("Are alive:");
+            Console.WriteLine("\t" + chuck.IsAlive);
+            Console.WriteLine("\t" + bruce.IsAlive);
+            Console.WriteLine("\t" + carl.IsAlive);
+            Console.WriteLine("\t" + dave.IsAlive);
 
-            //Console.WriteLine("Health:");
-            //Console.WriteLine("\t" + chuck.Health);
-            //Console.WriteLine("\t" + bruce.Health);
-            //Console.WriteLine("\t" + carl.Health);
-            //Console.WriteLine("\t" + dave.Health);
-
-
+            Console.WriteLine("Health:");
+            Console.WriteLine("\t" + chuck.Health);
+            Console.WriteLine("\t" + bruce.Health);
+            Console.WriteLine("\t" + carl.Health);
+            Console.WriteLine("\t" + dave.Health);
         }
     }
 }
