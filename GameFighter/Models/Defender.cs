d366531ce6dbaa71d2d2e5398605068234580af7
BadWolf03 <tspam765@gmail.com>
2022-02-07 22:15:29 +0200

    create class defender and test class rookie, create tests

---
+++ GameFighter/Models/Defender.cs
@@ -0,0 +1,22 @@
+﻿namespace GameFighter.Models
+{
+    public class Defender : Warrior
+    {
+        public int Defence { get; protected set; }
+
+        public Defender()
+        {
+            Health = 60;
+            Attack = 3;
+            Defence = 2;
+        }
+
+        public override void GetAttack(Warrior warrior)
+        {
+            if (warrior.Attack >= Defence)
+            {
+                Health -= (warrior.Attack - Defence);
+            }
+        }
+    }
+}
