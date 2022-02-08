233791b8699ef567e21cfe7d0d51b24a8d9697a7
BadWolf03 <tspam765@gmail.com>
2022-02-08 20:05:33 +0200

    create class Vampire

---
+++ GameFighter/Models/Vampire.cs
@@ -0,0 +1,14 @@
+﻿namespace GameFighter.Models
+{
+    public class Vampire : Warrior
+    {
+        public int Vampirism { get; protected set; }
+
+        public Vampire()
+        {
+            Health = 40;
+            Attack = 4;
+            Vampirism = 50;
+        }
+    }
+}
