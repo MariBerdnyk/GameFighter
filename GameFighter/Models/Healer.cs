ba58168ecbd1bb01672ce1eb5720724515d2fabc
BadWolf03 <tspam765@gmail.com>
2022-02-12 13:38:13 +0200

    make class healer

--- GameFighter/Models/Healer.cs
+++ GameFighter/Models/Healer.cs
@@ -1,6 +1,13 @@
 ﻿namespace GameFighter.Models
 {
-    public class Healer
+    public class Healer : Warrior
     {
+        public int Heal { get; protected set; }
+        public Healer()
+        {
+            Attack = 0;
+            Health = 60;
+            Heal = 2;
+        }
     }
 }
