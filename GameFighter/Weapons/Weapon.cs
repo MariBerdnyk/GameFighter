cc3d0347433606c9065785dfb2a6058ebfec838d
BadWolf03 <tspam765@gmail.com>
2022-02-16 17:44:04 +0200

    create weapon abstract class

---
+++ GameFighter/Weapons/Weapon.cs
@@ -0,0 +1,11 @@
+﻿namespace GameFighter.Weapons
+{
+    public abstract class Weapon
+    {
+        public int HealthParametr { get; init; }
+        public int AttackParametr { get; init; }
+        public int DefenceParametr { get; init; }
+        public int VampirismParametr { get; init; }
+        public int HealParametr { get; init; }
+    }
+}
