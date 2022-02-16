8c41d72a9431b8997f7987fdbb9884c3aa139e61
BadWolf03 <tspam765@gmail.com>
2022-02-16 17:52:29 +0200

    create great axe

---
+++ GameFighter/Weapons/GreatAxe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFighter.Weapons
+{
+    public class GreatAxe : Weapon
+    {
+        public GreatAxe()
+        {
+            HealthParametr = -15;
+            AttackParametr = 5;
+            DefenceParametr = -2;
+            VampirismParametr = 10;
+        }
+    }
+}
