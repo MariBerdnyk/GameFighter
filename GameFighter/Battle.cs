2136adab8b76f221069f05cf9c5d41bbed4c099b
BadWolf03 <tspam765@gmail.com>
2022-02-19 19:57:21 +0200

    validation

--- GameFighter/Battle.cs
+++ GameFighter/Battle.cs
@@ -6,7 +6,7 @@ namespace GameFighter
 {
     public static class Battle
     {
-        public static bool Fight(Warrior warrior1, Warrior warrior2, Army army1 = null, Army army2 = null)
+        private static void ValidatingUnits(Warrior warrior1, Warrior warrior2)
         {
             if (warrior1 == null || warrior2 == null)
             {
@@ -17,6 +17,34 @@ namespace GameFighter
             {
                 throw new ArgumentException($"{warrior1} and {warrior2} must be alive!");
             }
+
+            if (warrior1.Attack <= 0 && warrior2.Attack <= 0)
+            {
+                throw new Exception("First units must be able to attack!");
+            }
+        }
+
+        private static void ValidatingArmy(Army army1, Army army2)
+        {
+            if(army1 == null || army2 == null)
+            {
+                throw new ArgumentNullException("army1 and army2 must be initialised!");
+            }
+
+            if (army1.CountUnits == 0 || army2.CountUnits == 0)
+            {
+                throw new ArgumentException($"{army1} and {army2} must have units!");
+            }
+
+            if (!army1.HasAliveUnit || !army2.HasAliveUnit)
+            {
+                throw new ArgumentException($"{army1} and {army2} must have alive units!");
+            }
+        }
+
+        public static bool Fight(Warrior warrior1, Warrior warrior2, Army army1 = null, Army army2 = null)
+        {
+            ValidatingUnits(warrior1, warrior2);
 
             while (warrior1.IsAlive)
             {
@@ -35,25 +63,7 @@ namespace GameFighter
 
         public static bool Fight(Army army1, Army army2)
         {
-            if (army1 == null || army2 == null)
-            {
-                throw new ArgumentNullException("army1 and army2 must be initialised!");
-            }
-
-            if (army1.CountUnits == 0 || army2.CountUnits == 0)
-            {
-                throw new ArgumentException($"{army1} and {army2} must have units!");
-            }
-
-            if (!army1.HasAliveUnit || !army2.HasAliveUnit)
-            {
-                throw new ArgumentException($"{army1} and {army2} must have alive units!");
-            }
-
-            if (army1.ArmyMembers[0].Attack <= 0 && army2.ArmyMembers[0].Attack <= 0)
-            {
-                throw new Exception("First units must be able to attack!");
-            }
+            ValidatingArmy(army1, army2);
 
             army1.PrepareArmyForBattle();
             army2.PrepareArmyForBattle();
@@ -85,48 +95,12 @@ namespace GameFighter
                 }
             }
 
-            //foreach (var item in army1.ArmyMembers)
-            //{
-            //    if (item.IsAlive)
-            //    {
-            //        foreach (var item2 in army2.ArmyMembers)
-            //        {
-            //            if (item2.IsAlive && !Fight(item, item2, army1, army2))
-            //            {
-            //                army1.MoveUnits();
-            //                break;
-            //            }
-            //            else if (item2.IsAlive)
-            //            {
-            //                army2.MoveUnits();
-            //            }
-            //        }
-            //    }
-            //}
             return army1.HasAliveUnit;
         }
 
         public static bool StraightFight(Army army1, Army army2)
         {
-            if (army1 == null || army2 == null)
-            {
-                throw new ArgumentNullException("army1 and army2 must be initialised!");
-            }
-
-            if (army1.CountUnits == 0 || army2.CountUnits == 0)
-            {
-                throw new ArgumentException($"{army1} and {army2} must have units!");
-            }
-
-            if (!army1.HasAliveUnit || !army2.HasAliveUnit)
-            {
-                throw new ArgumentException($"{army1} and {army2} must have alive units!");
-            }
-
-            if (army1.ArmyMembers[0].Attack <= 0 && army2.ArmyMembers[0].Attack <= 0)
-            {
-                throw new Exception("First units must be able to attack!");
-            }
+            ValidatingArmy(army1, army2);
 
             army1.PrepareArmyForStraightBattle();
             army2.PrepareArmyForStraightBattle();
