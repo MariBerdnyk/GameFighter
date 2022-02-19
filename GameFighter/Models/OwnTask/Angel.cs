5b025f46eb49069060d9a415a70523de34d6fd8f
BadWolf03 <tspam765@gmail.com>
2022-02-19 16:58:48 +0200

    base class angel

--- GameFighter/Models/OwnTask/Angel.cs
+++ GameFighter/Models/OwnTask/Angel.cs
@@ -1,7 +1,31 @@
-﻿namespace GameFighter.Models.OwnTask
+﻿using GameFighter.Weapons;
+using System;
+
+namespace GameFighter.Models.OwnTask
 {
     public class Angel : Warrior
     {
+        public Angel()
+        {
+            Attack = 0;
+            DefaultAttack = 0;
+            Health = 1;
+            MaxHealth = 1;
+        }
 
+        public override int GetAttack(int attack)
+        {
+            return 0;
+        }
+
+        public override void Attacks(Warrior warrior, Army thisArmy)
+        {
+            Health--;
+        }
+
+        public override void EquipWeapon(Weapon weapon)
+        {
+            throw new InvalidOperationException("Angel can't have equipment!");
+        }
     }
 }
