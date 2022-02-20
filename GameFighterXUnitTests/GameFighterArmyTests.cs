eea946fa0ebc32d8a4a894b34a16f0bf14c88316
BadWolf03 <tspam765@gmail.com>
2022-02-20 14:52:29 +0200

    make parametrized tests

--- GameFighterXUnitTests/GameFighterArmyTests.cs
+++ GameFighterXUnitTests/GameFighterArmyTests.cs
@@ -1,107 +1,134 @@
 ﻿using GameFighter;
 using GameFighter.Models;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace GameFighterXUnitTests
 {
     public class GameFighterArmyTests
     {
-        [Fact]
-        public void Fight_TwoArmy3Knights_ReturnTrue()
+        public static IEnumerable<object[]> TestsInfoForTwo =>
+        new List<object[]>
+        {
+            new object[] { 3, 3, true },
+            new object[] { 3, 3, true },
+            new object[] { 1, 2, false },
+            new object[] { 2, 3, false },
+            new object[] { 5, 7, false },
+            new object[] { 20, 21, true },
+            new object[] { 10, 11, true },
+        };
+
+        public static IEnumerable<object[]> TestsInfoForThree =>
+        new List<object[]>
+        {
+            new object[] { 4, 1, 6, true },
+        };
+
+        [Theory]
+        [MemberData(nameof(TestsInfoForTwo))]
+        public void Fight_TwoArmy3Knights_ReturnTrue(int unitCount1, int unitCount2, bool expectedResult)
         {
             var army1 = new Army();
             var army2 = new Army();
 
-            army1.AddUnits<Knight>(3);
-            army2.AddUnits<Knight>(3);
+            army1.AddUnits<Knight>(unitCount1);
+            army2.AddUnits<Knight>(unitCount2);
 
-            Assert.True(Battle.Fight(army1, army2));
+            Assert.Equal(expectedResult, Battle.Fight(army1, army2));
         }
 
-        [Fact]
-        public void Fight_TwoArmy3Warriors_ReturnTrue()
+        [Theory]
+        [MemberData(nameof(TestsInfoForTwo))]
+        public void Fight_TwoArmy3Warriors_ReturnTrue(int unitCount1, int unitCount2, bool expectedResult)
         {
             var army1 = new Army();
             var army2 = new Army();
 
-            army1.AddUnits<Warrior>(3);
-            army2.AddUnits<Warrior>(3);
+            army1.AddUnits<Warrior>(unitCount1);
+            army2.AddUnits<Warrior>(unitCount2);
 
-            Assert.True(Battle.Fight(army1, army2));
+            Assert.Equal(expectedResult, Battle.Fight(army1, army2));
         }
 
-        [Fact]
-        public void Fight_TwoArmy1WarriorVs2Warriors_ReturnFalse()
+        [Theory]
+        [MemberData(nameof(TestsInfoForTwo))]
+        public void Fight_TwoArmy1WarriorVs2Warriors_ReturnFalse(int unitCount1, int unitCount2, bool expectedResult)
         {
             var army1 = new Army();
             var army2 = new Army();
 
-            army1.AddUnits<Warrior>(1);
-            army2.AddUnits<Warrior>(2);
+            army1.AddUnits<Warrior>(unitCount1);
+            army2.AddUnits<Warrior>(unitCount2);
 
-            Assert.False(Battle.Fight(army1, army2));
+            Assert.Equal(expectedResult, Battle.Fight(army1, army2));
         }
 
-        [Fact]
-        public void Fight_TwoArmy2WarriorVs3Warriors_ReturnFalse()
+        [Theory]
+        [MemberData(nameof(TestsInfoForTwo))]
+        public void Fight_TwoArmy2WarriorVs3Warriors_ReturnFalse(int unitCount1, int unitCount2, bool expectedResult)
         {
             var army1 = new Army();
             var army2 = new Army();
 
-            army1.AddUnits<Warrior>(2);
-            army2.AddUnits<Warrior>(3);
+            army1.AddUnits<Warrior>(unitCount1);
+            army2.AddUnits<Warrior>(unitCount2);
 
-            Assert.False(Battle.Fight(army1, army2));
+            Assert.Equal(expectedResult, Battle.Fight(army1, army2));
         }
 
-        [Fact]
-        public void Fight_TwoArmy5WarriorVs7Warriors_ReturnFalse()
+        [Theory]
+        [MemberData(nameof(TestsInfoForTwo))]
+        public void Fight_TwoArmy5WarriorVs7Warriors_ReturnFalse(int unitCount1, int unitCount2, bool expectedResult)
         {
             var army1 = new Army();
             var army2 = new Army();
 
-            army1.AddUnits<Warrior>(5);
-            army2.AddUnits<Warrior>(7);
+            army1.AddUnits<Warrior>(unitCount1);
+            army2.AddUnits<Warrior>(unitCount2);
 
-            Assert.False(Battle.Fight(army1, army2));
+            Assert.Equal(expectedResult, Battle.Fight(army1, army2));
         }
 
-        [Fact]
-        public void Fight_TwoArmy20WarriorVs21Warriors_ReturnTrue()
+        [Theory]
+        [MemberData(nameof(TestsInfoForTwo))]
+        public void Fight_TwoArmy20WarriorVs21Warriors_ReturnTrue(int unitCount1, int unitCount2, bool expectedResult)
         {
             var army1 = new Army();
             var army2 = new Army();
 
-            army1.AddUnits<Warrior>(20);
-            army2.AddUnits<Warrior>(21);
+            army1.AddUnits<Warrior>(unitCount1);
+            army2.AddUnits<Warrior>(unitCount2);
 
-            Assert.True(Battle.Fight(army1, army2));
+            Assert.Equal(expectedResult, Battle.Fight(army1, army2));
         }
 
-        [Fact]
-        public void Fight_TwoArmy10WarriorVs11Warriors_ReturnTrue()
+        [Theory]
+        [MemberData(nameof(TestsInfoForTwo))]
+        public void Fight_TwoArmy10WarriorVs11Warriors_ReturnTrue(int unitCount1, int unitCount2, bool expectedResult)
         {
             var army1 = new Army();
             var army2 = new Army();
 
-            army1.AddUnits<Warrior>(10);
-            army2.AddUnits<Warrior>(11);
+            army1.AddUnits<Warrior>(unitCount1);
+            army2.AddUnits<Warrior>(unitCount2);
 
-            Assert.True(Battle.Fight(army1, army2));
+            Assert.Equal(expectedResult, Battle.Fight(army1, army2));
         }
 
-        [Fact]
-        public void Fight_TwoArmy4Warrior1KnightVs6Warriors_ReturnFalse()
+        [Theory]
+        [MemberData(nameof(TestsInfoForThree))]
+        public void Fight_TwoArmy4Warrior1KnightVs6Warriors_ReturnFalse(int unitCount1, int unitCount2, int unitCount3, bool expectedResult)
         {
             var army1 = new Army();
             var army2 = new Army();
 
-            army1.AddUnits<Warrior>(4);
-            army1.AddUnits<Knight>(1);
-            army2.AddUnits<Warrior>(6);
+            army1.AddUnits<Warrior>(unitCount1);
+            army1.AddUnits<Knight>(unitCount2);
+            army2.AddUnits<Warrior>(unitCount3);
 
-            Assert.False(Battle.Fight(army1, army2));
+            Assert.Equal(expectedResult, Battle.Fight(army1, army2));
         }
 
         [Fact]
